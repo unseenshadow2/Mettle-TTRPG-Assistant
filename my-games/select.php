8b60f713c6560b501ee8454b45c035f9a79d83d8
wulfCode <gageroland@gmail.com>
2017-08-04 05:08:24 +0300

    PHP Update 1
    Now creates pages for each game, games are joinable, and game pages can be accessed by clicking on the campaign box

--- my-games/select.php
+++ my-games/select.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="select.css">
+        <link rel="stylesheet" type="text/css" href="/css/select.css">
         <title>Game Select</title>
     </head>
     <body>
@@ -19,7 +19,19 @@
                 </p>
                 <input type="text" name="user_role" >
         	<div class="form_Submit button1" onclick="document.forms['createGame'].submit();">Create</div>
-            <div class="form_Submit button2" onclick="document.forms['createGame'].submit();">Join</div>
+            </form>
+        </div>
+		<div class="form_Area form_Area_InputText">
+            <form name="joinGame" method="post" action="/form/joinGame.php">
+            	<p>
+                	Campaign Number
+                </p>
+                <input type="text" name="campaign_num" >
+            	<p>
+                	Your Role
+                </p>
+                <input type="text" name="j_role" >
+        	<div class="form_Submit button1" onclick="document.forms['joinGame'].submit();">Join</div>
             </form>
         </div>
         <?php
@@ -27,7 +39,8 @@
 		$username="";
 		$password="";
 		$dbname="";
-		$usertable="games";
+
+        session_start();
 
 		mysql_connect("$hostname","$username", "$password") or die ("<html><script language='JavaScript'>alert('Unable to connect to database! Please email contact@wulf.design'),history.go(-1)</script></html>");
 		mysql_select_db("$dbname");
@@ -37,13 +50,63 @@
 
         while($row = mysql_fetch_array($rs))
         {
-            echo "<div class='form_Area'>
+
+            /*$print = false;
+
+            //Check to see if we should print that module
+        	while($check1 = mysql_fetch_array($rs))
+        	{
+            	if ($_SESSION['USERNAME'] == $check1['userName'])
+                {
+                    $print = true;
+                }
+                while($check2 = mysql_fetch_array($rs2))
+                {
+                    if($_SESSION['USERNAME'] == $check2['j_userName'])
+                    {
+                        $print = true;
+                    }
+                }
+        	}
+
+            if ($print = true)
+            {*/
+            echo "	<a href = '" . $row['gameName'] . ".php'> <div class='form_Area'>
             		<p class='gameTitle'>" . $row['gameName'] . "</p>
                     <p class='gameID'> GameID: " . $row['gameNumber'] . "</p>
-                    <p class='gameUsers'>" . $row['userName'] . "</p>
-                  </div>
-";
-        }
+                    <p class='gameUsers'>" . $row['userName'] . " as " . $row['userType'] . " (Owner)</p>";
+
+            $strSQL2 = "SELECT * FROM gamePlayers WHERE '".$row['gameNumber']."' = j_gameNumber";
+        	$rs2 = mysql_query($strSQL2);
+            $i = 0;
+            while($row2 = mysql_fetch_array($rs2) )
+            {
+                echo "<p class='gameUsers'>-" . $row2['j_userName'] . " as " . $row2['j_userType'] . "</p>";
+                $i++;
+
+                if ($i > 6)
+                {
+                    echo "<p class='gameUsers'>+Plus More Users </p>";
+                    break;
+                }
+            }
+            echo " </div> </a>
+                 ";
+            $doc = fopen($row['gameName'] . ".php", "x+");
+            if ($doc)
+            {
+                $doc1 = fopen("template.php", "r");
+                while(!feof($doc1))
+                {
+                	fwrite($doc, fgets($doc1));
+                }
+                fclose($doc1);
+            }
+            fclose($doc);
+            }
+        /*}*/
+        mysql_close();
 		?>
+
     </body>
 </html>
