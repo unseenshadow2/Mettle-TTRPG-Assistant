680c850701339683a43544c634e4df37bd182cca
wulfCode <gageroland@gmail.com>
2017-08-04 05:04:25 +0300

    Template Example
    Template will be a php page that will be the play area for each campaign.

--- my-games/template.php
+++ my-games/template.php
@@ -1,1 +1,15 @@
-HEYYYYY
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="/css/my-games.css">
+		<title>GameTitle</title>
+    </head>
+    <body>
+        <?php session_start();?>
+        <div class="navBar">
+            <a class="rightMost" href="/index.php">Logout</a>
+            <a href="/my-games/select.php">Dashboard</a>
+			<?php echo "<p>".$_SESSION["USERNAME"]."</p>"?>
+        </div>
+    </body>
+</html>
